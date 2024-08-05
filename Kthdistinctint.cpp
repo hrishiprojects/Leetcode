a9585b62f95e20dce8590be4d48712cb2049b77b
hrishiprojects <142743430+hrishiprojects@users.noreply.github.com>
2024-08-05 12:15:04 -0400

    Add files via upload

---
+++ Kthdistinctint.cpp
@@ -0,0 +1,17 @@
+ unordered_map<string, int> mp;
+
+        for(string &s : arr) {
+            mp[s]++;
+        }
+
+        int count = 0;
+        for(string &s : arr) {
+            if(mp[s] == 1) {
+                count++;
+                if(count == k) {
+                    return s;
+                }
+            }
+        }
+
+        return "";
