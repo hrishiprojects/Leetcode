4242b460d7f45b90486094ce334d024e4cfe7faf
hrishiprojects <142743430+hrishiprojects@users.noreply.github.com>
2024-08-25 12:55:54 -0400

    Add files via upload

---
+++ Postorder.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+ void postorder(TreeNode* node, vector<int>& ans) {
+        if(node == NULL) return;
+        postorder(node->left, ans);
+        postorder(node->right, ans);
+        ans.push_back(node->val);
+    }
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int> ans;
+        postorder(root, ans);
+        return ans;
+    }
+};
